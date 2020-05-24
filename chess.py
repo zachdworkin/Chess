628fccbfe366447bf348eb9d5e07ddb0ca4b8506
Zach Dworkin <zachary.dworkin@gmail.com>
2020-05-23 19:20:24 -0700

    test update

--- chess.py
+++ chess.py
@@ -1,3 +1,8 @@
-'''This program is a 2 player chess game written by Zach Dworkin, Evan Dworkin, and Matthew Dworkin'''
+'''
+This program is a 2 player chess game written by Zach Dworkin, Evan Dworkin, and Matthew Dworkin
+press the green check to commit changes, then shift+command+k to push them to github, then click push
+to update master github branch
+'''
+
 
 from graphics import *
