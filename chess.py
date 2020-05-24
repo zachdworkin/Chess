7ebbbc95f5ea6f973892df659ebde912442716f6
Zach Dworkin <zachary.dworkin@gmail.com>
2020-05-23 19:18:58 -0700

    test update

--- chess.py
+++ chess.py
@@ -1,7 +1,3 @@
 '''This program is a 2 player chess game written by Zach Dworkin, Evan Dworkin, and Matthew Dworkin'''
 
 from graphics import *
-
-print("hello this is chess")
-print("test")
-print("test")
