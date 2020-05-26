d1aec1e6027e1cba3274b3c1a54de1acd8e1f256
Zach Dworkin <zachary.dworkin@gmail.com>
2020-05-25 20:25:26 -0700

    plan

--- chess.py
+++ chess.py
@@ -2,10 +2,7 @@
 This program is a 2 player chess game written by Zach Dworkin, Evan Dworkin, and Matthew Dworkin
 press the green check to commit changes, then shift+command+k to push them to github, then click push
 to update master github branch
-'''
 
-
-'''
 PLAN
 Methods:
     Location (row,column) = (0-7, 0-7)
@@ -15,18 +12,18 @@ Methods:
     Pieces
         classes, dictionary, hard code?
     Team
-    
+
 Possibility for Pieces:
     create a separate class for each king of piece ex:
-    
+
     class King:
-    
+
         def __init__(location, self):
             self.location = location
-    
+
         def possible_moves(self):#returns a list of possible move locations
 
-    
+
 '''
 
 from graphics import *
