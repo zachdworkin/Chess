62aa7277acd062af166e9e45d733a7abb2640e27
Zach Dworkin <zachary.dworkin@gmail.com>
2020-05-25 20:24:50 -0700

    plan

--- chess.py
+++ chess.py
@@ -5,7 +5,37 @@ to update master github branch
 '''
 
 
+'''
+PLAN
+Methods:
+    Location (row,column) = (0-7, 0-7)
+        create:
+            is_legal_move(args)
+            possible_moves(args)
+    Pieces
+        classes, dictionary, hard code?
+    Team
+    
+Possibility for Pieces:
+    create a separate class for each king of piece ex:
+    
+    class King:
+    
+        def __init__(location, self):
+            self.location = location
+    
+        def possible_moves(self):#returns a list of possible move locations
+
+    
+'''
+
 from graphics import *
 from g_interface import *
 
+class King:
+
+    def __init__(location, self):
+        self.location = location
+
+    def possible_moves(self):#returns a list of possible move locations
 
